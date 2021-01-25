aa4bf4b8615ef478bf6386119cbde55aea4cd137
DragosCocirlea <dragos.cocirlea@gmail.com>
2021-01-25 02:00:12 +0200

    added login

--- src/App.js
+++ src/App.js
@@ -1,148 +1,17 @@
-import React, { useState } from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet'
-import Modal from 'react-modal';
+import React from "react";
+import "./App.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/home/home";
+import Login from "./components/pages/Login";
 
-import UserLocation from './components/UserLocation';
-import IllegalParkingReports from './components/IllegalParkingReports';
-import FreeParkingLots from './components/FreeParkingLots';
-
-import Navbar from './components/Navbar';
-import "./App.css";
-
-import image_placeholder from './assets/placeholder.png';
-import current_location from './assets/current_location.png';
-
-
-Modal.setAppElement('#root');
-
-const App = () => {    
-    let [userModal, setUserModal] = useState(false);
-    let [illegalModal, setIllegalModal] = useState(false);
-    let [freeModal, setFreeModal] = useState(false);
-    let [lockView, setLockView] = useState(false);
-    
-    const [illegalProofSource, setIllegalProofSource] = useState("");
-    const handleIllegalProof = (target) => {
-        if (target.files) {
-            if (target.files.length !== 0) {
-                const file = target.files[0];
-                const newUrl = URL.createObjectURL(file);
-                setIllegalProofSource(newUrl);
-            }
-        }
-    }
-
-    const modalStyle = {
-        overlay: {
-            display: 'flex',
-            alignItems: 'center'
-        },
-        content: {
-            height: `fit-content`,
-            width: '80%',
-            position: 'absolute',
-            left: '50%',
-            top: '50%',
-            transform: 'translate(-50%, -50%)'
-        }
-    };
-
+function App() {
     return (
-        <div className="main-container">
-            <img
-                className="current-location-button"
-                src={current_location}
-                alt="center"
-                onClick={() => setLockView(true)}
-            ></img>
-
-            <MapContainer center={[44.45, 26.1]} zoom={15}>
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <UserLocation lockView={lockView} setLockView={setLockView}/>
-                <IllegalParkingReports />
-                <FreeParkingLots />
-            </MapContainer>
-            
-            <Navbar
-                user={setUserModal}
-                illegal={setIllegalModal}
-                proof={setIllegalProofSource}
-                free={setFreeModal}
-            />
-
-            {/* USER MODAL */}
-            <Modal isOpen={userModal} onRequestClose={() => setUserModal(false)} style={modalStyle}>
-                <div className="modalDiv">
-                    <h2 className="modalTitle">Welcome user!</h2>
-                    <div className="buttonDiv">
-                        <button>Logout</button>
-                    </div>
-                </div>
-                
-                <div className="buttonDiv closeButtonDiv">
-                    <button onClick={() => setUserModal(false)}>Close</button>
-                </div>
-            </Modal>
-
-            {/* ILLEGAL PARKING MODAL */}
-            <Modal isOpen={illegalModal} onRequestClose={() => setIllegalModal(false)} style={modalStyle}>
-                <div className="modalDiv">
-                    <h2 className="modalTitle">Report illegal parking at current location</h2>
-                                    
-                    <div className="descriptionDiv">
-                        <p>Description</p>
-                        <textarea></textarea>
-                    </div>                
-
-                    <div className="pictureDiv">
-                        <p>Take a picture</p>
-                        {illegalProofSource ? 
-                            <img src={illegalProofSource} alt="snap"></img>
-                            :
-                            <img src={image_placeholder} alt="snap"></img>
-                        }
-                        <input
-                            accept='image/*'
-                            type='file'
-                            capture='environment'
-                            onChange={(e) => handleIllegalProof(e.target)}
-                        />
-                    </div>
-
-                    <div className="buttonDiv">
-                        <button>Send report</button>
-                    </div>
-                </div>
-
-                <div className="buttonDiv closeButtonDiv">
-                    <button onClick={() => setIllegalModal(false)}>Close</button>
-                </div>
-            </Modal>
-
-            {/* FREE PARKING MODAL */}
-            <Modal isOpen={freeModal} onRequestClose={() => setFreeModal(false)} style={modalStyle}>
-                <div className="modalDiv">
-                    <h2 className="modalTitle">Report free parking spot at current location</h2>
-                    
-                    <div className="descriptionDiv">
-                        <p>Description</p>
-                        <textarea></textarea>
-                    </div>                
-
-
-                    <div className="buttonDiv">
-                        <button>Send report</button>
-                    </div>
-                </div>
-
-                <div className="buttonDiv closeButtonDiv">
-                    <button onClick={() => setFreeModal(false)}>Close</button>
-                </div>
-            </Modal>
-
-        </div>
+        <Router>
+            <Route path="/" exact component={Login} />
+            <Switch>
+                <Route path="/home" exact component={Home} />
+            </Switch>
+        </Router>
     );
 }
 
